c56762cda2353ad2308a46580803b3eeaa005551
TheKingbuildboot <77891371+TheKingbuildboot@users.noreply.github.com>
2021-04-27 10:43:35 -0300

    Add support for dotOS

--- dot_rav.mk
+++ dot_rav.mk
@@ -19,7 +19,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Inherit from common.mk
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
@@ -27,7 +27,7 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 PRODUCT_BRAND := Motorola
 PRODUCT_DEVICE := rav
 PRODUCT_MANUFACTURER := Motorola
-PRODUCT_NAME := lineage_rav
+PRODUCT_NAME := dot_rav
 PRODUCT_MODEL := motog(8)
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
